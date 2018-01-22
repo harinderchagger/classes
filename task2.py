280661947c745031d5f2a7d81d8c2885665b36dd
harinder.chagger <harinder.chagger@dal.ca>
2018-01-22 15:34:26 -0400

    my first class

---
+++ task2.py
@@ -0,0 +1,20 @@
+import math
+class point():
+    '''  '''
+
+blank = point()
+class rectangle():
+    ""
+
+object1 = rectangle()
+object1.width = 100
+object1.height = 200
+blank.x,blank.y = 0,0
+
+def find_center(e,a,b,c,d):
+    cntrX = a + 1/2*(b)
+    cntrY = c + 1/2*(d)
+    print(cntrX)
+    print(cntrY)
+find_center(rectangle,blank.x,object1.width,blank.y,object1.height)
+
